688ebedcf518ba0aa464d5ce4fd9a52a1f8890b8
HarryVerhoef <harry.verhoef1999@gmail.com>
2021-04-11 16:40:41 +0100

    Remove redundant functio

--- chessfront/src/components/MoveSequence/index.jsx
+++ chessfront/src/components/MoveSequence/index.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from "react-redux";
 
-const movePiece = (sequence, dispatch, i) => {
-  
-};
-
 const MoveSequence = ({ sequence, timeStep }) => {
   const dispatch = useDispatch();
   useEffect(() => {
